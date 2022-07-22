f52eb019fe95676f768c92df3feeb26726cecbb5
Sunil-731820 <sunilkumargupta1170@gmail.com>
2022-07-22 16:36:36 +0530

    updated again

--- app.js
+++ app.js
@@ -11,13 +11,18 @@
    
 // }).listen(8080)
 
-var calc = require('./calc.js')
-result = calc.add(4,5)
-console.log("the sum of the two Number is "+ result)
-var result1 = calc.add(10,10)
-console.log("again the sum of the two Number is "+result1)
-console.log("the subtraction of the two Number is ")
-var sub1 = calc.sub(10,10)
-console.log("the subtraction fo the two Number is "+ sub1)
-var subtr1 = calc.sub(100,1000)
-console.log("the subtraction of the Two Number is "+ subtr1)
+// var calc = require('./calc.js')
+// result = calc.add(4,5)
+// console.log("the sum of the two Number is "+ result)
+// var result1 = calc.add(10,10)
+// console.log("again the sum of the two Number is "+result1)
+// console.log("the subtraction of the two Number is ")
+// var sub1 = calc.sub(10,10)
+// console.log("the subtraction fo the two Number is "+ sub1)
+// var subtr1 = calc.sub(100,1000)
+// console.log("the subtraction of the Two Number is "+ subtr1)
+
+var fs = require('fs')
+fs.readFile('calc.js','utf8',function(err,data){
+    console.log(data)
+})
